b989f1718e42e25cb0fc85ea5929bc740fc4edd6
Ville Peurala <ville.peurala@gmail.com>
2013-10-05 13:04:56 +0300

    Color reduction

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
+#include <opencv2/core/cvdef.h>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
+void colorReduce(cv::Mat& image, int div = 64) {
+	int nl = image.rows;
+	int nc = image.cols * image.channels();
+
+	for (int i = 0; i < nl; i++) {
+		uchar* data = image.ptr<uchar>(i);
+
+		for (int j = 0; j < nc; j++) {
+			data[j] = data[j] / div * div + div / 2;
+		}
+	}
+}
+
 int main(int argc, char** argv) {
-	if (argc != 2) {
-		std::cerr << "Incorrect number of command line arguments (1 expected): "
+	if (argc != 3) {
+		std::cerr << "Incorrect number of command line arguments (2 expected): "
 				<< (argc - 1) << "\n";
 		return 1;
 	}
 	std::cout << "Opening image" << argv[1] << "\n";
 	cv::Mat image = cv::imread(argv[1]);
 	std::cout << "Image opened, size: " << image.size << "\n";
+	colorReduce(image);
+	std::cout << "Writing color reduced version to " << argv[2] << "\n";
+	cv::imwrite(argv[2], image);
 	return 0;
 }
