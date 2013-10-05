39d6c07635b8502a647cb4e87df07e8cc4f8d8fa
Ville Peurala <ville.peurala@gmail.com>
2013-10-05 13:15:24 +0300

    Refactoring.

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,21 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
-void colorReduce(cv::Mat& image, int div = 64) {
-	int nl = image.rows;
-	int nc = image.cols * image.channels();
+const cv::Mat colorReduce(const cv::Mat& input, int div = 64) {
+	cv::Mat output = input.clone();
+
+	int nl = output.rows;
+	int nc = output.cols * output.channels();
 
 	for (int i = 0; i < nl; i++) {
-		uchar* data = image.ptr<uchar>(i);
+		uchar* data = output.ptr<uchar>(i);
 
 		for (int j = 0; j < nc; j++) {
 			data[j] = data[j] / div * div + div / 2;
 		}
 	}
+
+	return output;
 }
 
 int main(int argc, char** argv) {
@@ -22,11 +26,13 @@ int main(int argc, char** argv) {
 				<< (argc - 1) << "\n";
 		return 1;
 	}
-	std::cout << "Opening image" << argv[1] << "\n";
-	cv::Mat image = cv::imread(argv[1]);
-	std::cout << "Image opened, size: " << image.size << "\n";
-	colorReduce(image);
-	std::cout << "Writing color reduced version to " << argv[2] << "\n";
-	cv::imwrite(argv[2], image);
+	const char* inputFileName = argv[1];
+	const char* outputFileName = argv[2];
+	std::cout << "Opening image" << inputFileName << "\n";
+	cv::Mat input = cv::imread(inputFileName);
+	std::cout << "Image opened: " << inputFileName << "\n";
+	cv::Mat output = colorReduce(input);
+	std::cout << "Writing color reduced version to " << outputFileName << "\n";
+	cv::imwrite(outputFileName, output);
 	return 0;
 }
